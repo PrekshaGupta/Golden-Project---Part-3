aa0ccb04ec4cbe043a1c96992bb9b8298d23b0ca
PrekshaGupta <69909564+PrekshaGupta@users.noreply.github.com>
2021-01-28 21:59:06 +0530

    Add files via upload

---
+++ Level2.js
@@ -0,0 +1,18 @@
+function level2(){
+ background(backdropImg2);
+
+ 
+}
+
+/*function preload(){
+ backdropImg2 = loadImage("images/rainforest.jpg");
+}
+
+function setup(){
+ createCanvas(1000,600);
+}
+
+function draw(){
+ background(backdropImg2);
+ drawSprites();
+}*/
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Golden-Project---Part-3
+# p5.play-boilerplate
+Boiler plate for p5.play
